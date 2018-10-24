e95213c85083f1b6abf0db5ee3e393ab78b41a60
jussi <jussi@lingvi.st>
2018-10-24 14:04:30 +0200

    first take

--- cases.py
+++ cases.py
@@ -1,2 +1,28 @@
-# take a set of words
 import morphology
+import hyperdimensionalsemanticspace
+
+space = hyperdimensionalsemanticspace.SemanticSpace(2000, 10)
+for item in morphology.lexicon:
+    space.additem(item, False)
+for item in morphology.casus:
+    space.additem(item, False)
+for item in morphology.clitics:
+    space.additem(item, False)
+for item in morphology.possessives:
+    space.additem(item, False)
+for item in morphology.numerus:
+    space.additem(item, False)
+
+# take a set of texts
+# for each text, inspect to see if a word we know about appears
+text = "a b c"
+for word in text:
+    if morphology.known(word):
+        r = morphology.lookup(word)
+
+
+
+
+# adjust items in space by cooccurrence
+# for each item in space, output closest neighbours, separated by type (lexeme, number, case, possessive clitic)
+
--- morphology.py
+++ morphology.py
@@ -1,4 +1,8 @@
 dictionary = {}
+clitics = {}
+possessives = {}
+numerus = {}
+casus = {}
 
 def known(word):
     if word in dictionary:
@@ -11,3 +15,14 @@ def read_dictionary():
     global dictionary
     dictionary = {}
 
+def lookup(word):
+    if known(word):
+        r = {}
+        r["lemma"] = dictionary[word]
+        r["luku"] = numerus[word]
+        r["sija"] = casus[word]
+        r["omistus"] = possessives[word]
+        r["liite"] = clitics[word]
+        return r
+    else:
+        return None
