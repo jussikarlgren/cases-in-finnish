ccea472d6fa001e8da1a6a364c3a4cc82ca1b661
jussi <jussi@lingvi.st>
2019-01-08 12:25:12 +0100

    added variable (used for logger output now no longer relevant but)

--- cases.py
+++ cases.py
@@ -4,12 +4,20 @@ from nltk import sent_tokenize
 from nltk import word_tokenize
 import readrawtext
 import time
+import sparsevectors
+import json
 
+full = False  #  set to true for full word space (very expensive)
+probes = True  # set to true to get an analysis of probe term space
+profiles = False  # to get a profile similarity for lemmas wrt observed cases
+
+debug = True
 
 def nop(dummy):
     # do nothing
     return None
 
+outputfile = "/home/jussi/finnish-cases-output.txt"
 
 dimensionality = 2000
 denseness = 10
@@ -18,6 +26,10 @@ denseness = 10
 tokencontextspace = hyperdimensionalsemanticspace.SemanticSpace(dimensionality, denseness, "token vs wds, 2x2")
 # tokens x words context sentence
 tokenutterancespace = hyperdimensionalsemanticspace.SemanticSpace(dimensionality, denseness, "token vs wds, utt")
+# tokens x words context 2x2
+fullcontextspace = hyperdimensionalsemanticspace.SemanticSpace(dimensionality, denseness, "all token vs wds, 2x2")
+# tokens x words context sentence
+fullutterancespace = hyperdimensionalsemanticspace.SemanticSpace(dimensionality, denseness, "all token vs wds, utt")
 # lemmas x cases context one token per entire corpus
 lemmacasespace = hyperdimensionalsemanticspace.SemanticSpace(dimensionality, denseness, "lemma x case")
 # lemmas x cases per text
@@ -44,11 +56,18 @@ readrawtext.readstats()
 window = 2
 files = readrawtext.getfilelist()
 i = 0
+j = 0
+k = 0
 antalsatser = 0
 antalord = 0
 threshold = 0.1
+seen = set()
 for file in files:
+    with open(outputfile, "a") as f:
+        f.write(f'{i}\t{j}\t{k}\t{file}\t{full}\t{probes}\t{profiles}\n')
     i += 1
+    j += 1
+    k += 1
     texts = readrawtext.doonetextfile(file)
     antalsatser += len(texts)
     flag = []
@@ -56,15 +75,26 @@ for file in files:
         ss = sent_tokenize(text.lower())
         for sent in ss:
             words = word_tokenize(sent)
-            i = 0
+            ii = 0
             for word in words:
-                i += 1
+                ii += 1
+                lhs = words[ii - window:ii]
+                rhs = words[ii + 1:ii + window + 1]
+                if full:
+                    fullcontextspace.observe(word)
+                    fullutterancespace.observe(word)
+                    for lw in lhs:
+                        fullcontextspace.addintoitem(word, lw, readrawtext.weight(word), "before")
+                    for rw in rhs:
+                        fullcontextspace.addintoitem(word, rw, readrawtext.weight(word), "after")
+                    for otherword in words:
+                        fullutterancespace.addintoitem(word, otherword, readrawtext.weight(otherword))
                 if morphology.known(word):
                     flag.append(word)
+                    seen.add(word)
                     featureset = morphology.lookup(word)
-                    lemma = featureset["lemma"]
-                    lemmacasespace.observe(lemma)
                     thesefeatures = []
+                    lemma = featureset["lemma"]
                     for dd in ["case", "num", "poss"]:
                         if dd in featureset:
                             featurecontextspace.observe(featureset[dd], True, dd)
@@ -75,71 +105,127 @@ for file in files:
                             featurecontextspace.observe(cc, True, "clitic")
                             featurecollocationspace.observe(cc, True, "clitic")
                             thesefeatures.append(cc)
-                    for fff in thesefeatures:
-                        lemmacasespace.addintoitem(lemma, fff)
-                        for eee in thesefeatures:
-                            if fff != eee:
-                                featurecollocationspace.addintoitem(fff, eee)
-                    tokencontextspace.observe(word, True, lemma)
-                    lhs = words[i-window:i]
-                    rhs = words[i+1:i+window+1]
-                    for lw in lhs:
-                        tokencontextspace.addintoitem(word, lw, readrawtext.weight(word), "before")
+                    if profiles:
+                        lemmacasespace.observe(lemma)
                         for fff in thesefeatures:
-                            featurecontextspace.addintoitem(fff, lw, 1, "before")
-                    for rw in rhs:
-                        tokencontextspace.addintoitem(word, rw, readrawtext.weight(word), "after")
-                        for fff in thesefeatures:
-                            featurecontextspace.addintoitem(fff, rw, 1, "after")
+                            lemmacasespace.addintoitem(lemma, fff)
+                            for eee in thesefeatures:
+                                if fff != eee:
+                                    featurecollocationspace.addintoitem(fff, eee)
+                    if probes:
+                        tokencontextspace.observe(word, True, lemma)
+                        for lw in lhs:
+                            tokencontextspace.addintoitem(word, lw, readrawtext.weight(word), "before")
+                            for fff in thesefeatures:
+                                featurecontextspace.addintoitem(fff, lw, 1, "before")
+                        for rw in rhs:
+                            tokencontextspace.addintoitem(word, rw, readrawtext.weight(word), "after")
+                            for fff in thesefeatures:
+                                featurecontextspace.addintoitem(fff, rw, 1, "after")
             if len(flag) >= 1:
                 antalord += len(words)
-            for knownword in flag:
-                featureset = morphology.lookup(knownword)
-                thesefeatures = []
-                for dd in ["case", "num", "poss"]:
-                    if dd in featureset:
-                        featureutterancespace.observe(featureset[dd], True, dd)
-                        thesefeatures.append(featureset[dd])
-                for cc in clitics:
-                    if cc in featureset:
-                        featureutterancespace.observe(cc, True, "clitic")
-                        thesefeatures.append(cc)
-                tokenutterancespace.observe(knownword, True, featureset["lemma"])
-                for word in words:
-                    tokenutterancespace.addintoitem(knownword, word)
-                    for fff in thesefeatures:
-                        featureutterancespace.addintoitem(fff, word)
+            if probes:
+                for knownword in flag:
+                    featureset = morphology.lookup(knownword)
+                    thesefeatures = []
+                    for dd in ["case", "num", "poss"]:
+                        if dd in featureset:
+                            featureutterancespace.observe(featureset[dd], True, dd)
+                            thesefeatures.append(featureset[dd])
+                    for cc in clitics:
+                        if cc in featureset:
+                            featureutterancespace.observe(cc, True, "clitic")
+                            thesefeatures.append(cc)
+                    tokenutterancespace.observe(knownword, True, featureset["lemma"])
+                    for word in words:
+                        tokenutterancespace.addintoitem(knownword, word, readrawtext.weight(word))
+                        for fff in thesefeatures:
+                            featureutterancespace.addintoitem(fff, word)
             flag = []
-    if i > 3:
-        i = 0
-        print("==", time.ctime(), "==", antalsatser, "==", antalord, "============",  sep="\t")
-        for s in [tokencontextspace, tokenutterancespace]:
-            print(s.name, "----------", sep="\t")
-            tokenneighbours = {}
-            nabesize = {}
-            for oneitem in s.contextspace:
-                if s.tag[oneitem] not in tokenneighbours:
-                    tokenneighbours[s.tag[oneitem]] = 0
-                    nabesize[s.tag[oneitem]] = 0
-                nabe = s.contextneighbours(oneitem, 0, True, True)
-                for nnn in nabe:
-                    tokenneighbours[s.tag[oneitem]] += nnn[1] / ((len(nabe) + 1) * len(nabe))
-                    if nnn[1] > threshold:
-                        nabesize[s.tag[oneitem]] += 1 / len(nabe)
-            for lemma in tokenneighbours:
-                print(lemma, tokenneighbours[lemma], nabesize[lemma], sep="\t")
 
+#        for s in [tokencontextspace, tokenutterancespace]:
+            # number of items in nabe (observed morphset)
+            # number of other items with nabe radius
+            # average distance ============from items to each other
+            # average distance from centroid to all items
+            # some good examples of neighbours outside morphset
+            # potentially see if morphset can be separated into subsets based on geometry
+    if i > 3 and probes:
+        i = 0
+        with open(outputfile, "a") as f:
+            f.write(
+                f'==\t{time.ctime()}\t==\t{antalsatser}\t==\t{antalord}\t============\t{full}\t{probes}\t{profiles}\n')
+            for s in [tokencontextspace, tokenutterancespace]:
+                f.write(s.name + "\t" + "----------\n")
+                morphsetaveragedistance = {}
+                morphsetaveragethresholdeddistance = {}
+                morphsetaveragethresholdedsize = {}
+                morphsetcentroiddistance = {}
+                morphsetvectors = {}
+                morphsetsize = {}
+                morphset = {}
+                morphsetcache = {}
+                lemmafrequency = {}
+                seenlemmas = []
+                for oneitem in s.contextspace:
+                    lemma = s.tag[oneitem]
+                    if lemma not in seenlemmas:
+                        seenlemmas.append(lemma)
+                        morphsetaveragedistance[lemma] = 0
+                        morphsetaveragethresholdedsize[lemma] = 0
+                        morphsetaveragethresholdeddistance[lemma] = 0
+                        morphsetcentroiddistance[lemma] = 0
+                        morphsetvectors[lemma] = []
+                        morphsetsize[lemma] = 0
+                        morphset[lemma] = []
+                        morphsetcache[lemma] = {}
+                        lemmafrequency[lemma] = 0
+                    lemmafrequency[lemma] += s.observedfrequency[oneitem]
+                    morphset[lemma] = s.contextneighbours(oneitem, 0, True, True)  # get items with same lemma
+                    morphsetsize[lemma] = len(morphset[lemma])
+                    morphsetcache[lemma][oneitem] = morphset[lemma]
+                    for nnn in morphset[lemma]:
+                        morphsetvectors[lemma].append(s.contextspace[nnn[0]])
+                        morphsetaveragedistance[lemma] += nnn[1]
+                        if nnn[1] > threshold:
+                            morphsetaveragethresholdedsize[lemma] += 1
+                            morphsetaveragethresholdeddistance[lemma] += nnn[1]
+                for observedlemma in seenlemmas:
+                    a = 0
+                    if morphsetsize[observedlemma] > 0:
+                        a = morphsetaveragedistance[observedlemma] / (morphsetsize[observedlemma] *
+                                                                      (morphsetsize[observedlemma] + 1))
+                    c = sparsevectors.centroid(morphsetvectors[observedlemma])
+                    m = sparsevectors.averagedistance(c, morphsetvectors[observedlemma], debug)
+                    d = 0
+                    if observedlemma in morphsetaveragethresholdedsize and \
+                            morphsetaveragethresholdedsize[observedlemma] > 1:
+                        d = morphsetaveragethresholdeddistance[observedlemma] / \
+                            morphsetaveragethresholdedsize[observedlemma]
+                    f.write(f"{observedlemma}\t{lemmafrequency[observedlemma]}\t{a}\t{m}\t{morphsetsize[observedlemma]}\t{d}\n")
+                    f.write(f"\t\t\t{morphsetcache[observedlemma]}\n")
+    if j > 3 and full:
+        print("..", time.ctime(), "..", antalsatser, "..", antalord, "..........", full, probes, profiles, sep="\t")
+        j = 0
+        for s in [fullcontextspace, fullutterancespace]:
+            print(s.name)
+            for item in seen:
+                nabe = s.contextneighbours(item, 10, True)
+                print(item, nabe)
+    if k > 3 and profiles:
+        print("##", time.ctime(), "##", antalsatser, "##", antalord, "###", full, probes, profiles, sep="\t")
+        k = 0
         for s in [lemmacasespace]:
             print(s.name)
             for oneitem in s.contextspace:
-                print(oneitem, s.contextneighbours(oneitem), sep="\t")
-
-        for s in [featurecontextspace, featureutterancespace]:
-            print(s.name)
-            for oneitem in s.contextspace:
-                print(oneitem, s.contextneighbours(oneitem, 0, True, True, 0), sep="\t")
-
-        for s in [featurecollocationspace]:
-            print(s.name)
-            for oneitem in s.contextspace:
-                print(oneitem, s.contextneighbours(oneitem, 0, True, False, 0), sep="\t")
+                print(oneitem, s.observedfrequency[oneitem], s.contextneighbours(oneitem, 10, True), sep="\t")
+#        for s in [featurecollocationspace]:
+#            json.dump(s.name)
+#            for oneitem in s.contextspace:
+#                json.dump(oneitem, s.contextneighbours(oneitem, 0, True, False, 0), sep="\t")
+#
+#        for s in [featurecontextspace, featureutterancespace]:
+#            json.dump(s.name)
+#            for oneitem in s.contextspace:
+#                json.dump(oneitem, s.contextneighbours(oneitem, 0, True, True, 0), sep="\t")
+#
